74feaa48534e8883eccf0f8cd330aea9a57dc7d4
hoborific <hoborific09@gmail.com>
2019-01-21 21:47:49 +1100

    attempts #3

--- src/main/java/dyeablechicken/common/MyGenetics.java
+++ src/main/java/dyeablechicken/common/MyGenetics.java
@@ -1,14 +1,24 @@
 package dyeablechicken.common;
 
 
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.data.DataTracker;
+import net.minecraft.entity.data.TrackedData;
+import net.minecraft.entity.data.TrackedDataHandlerRegistry;
+
 public class MyGenetics {
 
     Object myself;
     public Boolean hasGenetics;
     protected int[] genetics; //new int[]{}
+    public static TrackedData<String> GENETIC_TRACKER;
+    public boolean registeredTracker = false;
+    static{GENETIC_TRACKER = DataTracker.registerData(Entity.class, TrackedDataHandlerRegistry.STRING);}
+
 
     public MyGenetics(Object en){
         this.myself = en;
+
     }
 
     public int[] getGenetics(){
--- src/main/java/dyeablechicken/common/mixin/EntityEvents.java
+++ src/main/java/dyeablechicken/common/mixin/EntityEvents.java
@@ -3,10 +3,14 @@ package dyeablechicken.common.mixin;
 import dyeablechicken.common.IGeneticBase;
 import dyeablechicken.common.MyGenetics;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.data.DataTracker;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.util.Hand;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -18,6 +22,11 @@ import java.util.Random;
 @Mixin(Entity.class)
 public class EntityEvents implements IGeneticBase {
     MyGenetics myGenes = new MyGenetics(this);
+    @Shadow
+    protected DataTracker dataTracker;
+    @Shadow
+    public World world;
+
 
     @Inject(at = @At("RETURN"), method = "toTag")
     public void toTag(CompoundTag tag, CallbackInfoReturnable cir) {
@@ -32,18 +41,28 @@ public class EntityEvents implements IGeneticBase {
         }
 
     }
+    @Inject(at = @At("RETURN"), method = "<init>", cancellable = true)
+    public void init(EntityType<?> entityType_1, World world_1, CallbackInfo ci) {
+        dataTracker.startTracking(MyGenetics.GENETIC_TRACKER, "test");
+    }
 
     @Inject(at = @At("RETURN"), method = "fromTag", cancellable = true)
     public void fromTag(CompoundTag tag, CallbackInfo ci) {
-        if(tag.getBoolean("dyeablechicken:hasGenetics")){
+        if (tag.getBoolean("dyeablechicken:hasGenetics")) {
             myGenes.setGenetics(tag.getIntArray("dyeablechicken:genes"));
             myGenes.hasGenetics = tag.getBoolean("dyeablechicken:hasGenetics");
             //System.out.println("Loaded from tag Genetics " + Arrays.toString(myGenes.getGenetics()));
-        }else{
+        } else {
             //System.out.println("Nothing saved, skipping load");
+
         }
     }
 
+    public int getIntGenetics(){
+        int[] test = myGenes.getGenetics();
+        if(test == null){test = new int[]{1,1,1};}
+        return test[0]+test[1]+test[2];
+    }
     @Override
     public void initializeGenetics() {
         Random randy = new Random();
@@ -53,12 +72,18 @@ public class EntityEvents implements IGeneticBase {
             //System.out.println("INITIALIZED GENETICS: " + Arrays.toString( myGenes.getGenetics()));
         }
     }
-    @Inject(at = @At("RETURN"), method = "interact", cancellable = true)
+   /* @Inject(at = @At("RETURN"), method = "interact", cancellable = true)
     public boolean interact(PlayerEntity playerEntity_1, Hand hand_1, CallbackInfoReturnable cir){
         if(!playerEntity_1.getEntityWorld().isClient()){
             System.out.println("interact: "+ Arrays.toString(myGenes.getGenetics()));
         }return true;
-    }
+    }*/
+   @Inject(at = @At("RETURN"), method = "interact", cancellable = true)
+   public boolean interact(PlayerEntity playerEntity_1, Hand hand_1, CallbackInfoReturnable cir){
+           System.out.println("interact: "+ Arrays.toString(myGenes.getGenetics()));
+           System.out.println(dataTracker.get(MyGenetics.GENETIC_TRACKER));
+       return false;
+   }
     @Override
     public int[] getGenes() {
         return myGenes.getGenetics();
