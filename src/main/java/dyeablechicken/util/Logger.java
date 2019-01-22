a17970c43574c156f4035340743bf4306a1aa5c5
ryougishiki0001 <cevkiv@gmail.com>
2019-01-21 20:26:58 -0500

    Until fabric gets something for prepending mod names to system out, just import dyeablechicken.util.Logger.log as static and use log("whatever")

---
+++ src/main/java/dyeablechicken/util/Logger.java
@@ -0,0 +1,9 @@
+package dyeablechicken.util;
+
+public class Logger {
+    static private String ModName = "Dyeable Chicken";
+
+    public static void log(String data) {
+        System.out.println("[" + ModName + "] " + data);
+    }
+}
