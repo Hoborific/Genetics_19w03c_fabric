a874bf5bc6890ae165e583a86f609ae208c4376d
ryougishiki0001 <cevkiv@gmail.com>
2019-01-23 18:49:03 -0500

    Changed: tooltips now build in item via overriding the buildTooptip function instead of via mixin.

--- src/main/java/dyeablechicken/items/GeneticsBaseItem.java
+++ src/main/java/dyeablechicken/items/GeneticsBaseItem.java
@@ -1,9 +1,16 @@
 package dyeablechicken.items;
 
+import net.minecraft.client.item.TooltipOptions;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.TextComponent;
+import net.minecraft.world.World;
+
+import java.util.List;
 
 public abstract class GeneticsBaseItem extends Item  {
     public GeneticsBaseItem(Settings item$Settings_1) {
         super(item$Settings_1);
     }
+
 }
