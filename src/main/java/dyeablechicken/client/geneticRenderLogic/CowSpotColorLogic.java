98045b63f60c2b2ca880884554e361f8d43c4c9c
hoborific <hoborific09@gmail.com>
2019-01-25 14:51:22 +1100

    palceholder functions for default cow spawn genetic examples

--- src/main/java/dyeablechicken/client/geneticRenderLogic/CowSpotColorLogic.java
+++ src/main/java/dyeablechicken/client/geneticRenderLogic/CowSpotColorLogic.java
@@ -17,17 +17,15 @@ public class CowSpotColorLogic extends BaseColorLogic {
 
     @Override
     public float[] geneticsToRGB(Entity en) {
-        //float[] rgbAsFloat = intToRGBFloat(); // 30-255 greyscaled
-        //System.out.println(rgbAsFloat[0] + " t" +rgbAsFloat[1] + " t"+rgbAsFloat[2] + " t");
-        //return rgbAsFloat;
         float greyscale = 13 + (13 * getValueOfPrimaryGene(en));
-        float brown = 1;
         int secondary = getValueofSecondaryGene(en);
-        if (secondary >= 4) {
+        float brown = 1;
+        if (secondary >= 4) { //apply brown spots to weak genes rather than greyscale (darker means brown now)
             brown = (float) (3 * secondary);
         }
         if (secondary >= 7) {
-            greyscale += 100;
+            greyscale += 100; //increase RGB base if strong genes
+            brown = brown / 2; //half brown(RED) pigment to not tint the white
         }
         return new float[]{(greyscale + brown) / 255.0f, greyscale / 255.0f, greyscale / 255.0f};
     }
